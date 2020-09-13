03e5acd11b790d9c656478358fcd4db21579a80c
avdim <avdim@mail.ru>
2020-09-13 11:26:04 +0300

    mvi telegram

--- src/jvmMain/kotlin/com/sample/scope.kt
+++ src/jvmMain/kotlin/com/sample/scope.kt
@@ -3,7 +3,8 @@ package com.sample
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 
 actual fun launchCoroutineDirty(block: suspend () -> Unit) {//todo
-    GlobalScope.launch(Dispatchers.Main) { block() }
+    GlobalScope.launch(newSingleThreadContext("mySingleThreadContext")) { block() }
 }
